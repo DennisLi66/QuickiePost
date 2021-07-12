93fbd45ea5f13b7d79de4a5efcde6a4f8104839e
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-07-12 14:10:26 -0700

    Basic Navbar Implementation
    Flesh out navbar
    get posts and display them

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
 import logo from './logo.svg';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+// import Button from 'react-bootstrap/Button'
 import './App.css';
 
+//things ill Need
+//simple navbar
+//onload produce Posts
+
 function App() {
+
+
+
+
   return (
     <div className="App">
+    <Navbar bg="light" expand="lg">
+  <Navbar.Brand>QuickiePost</Navbar.Brand>
+  <Navbar.Toggle aria-controls="navbarScroll" />
+  <Navbar.Collapse id="navbarScroll">
+    <Nav
+      className="mr-auto my-2 my-lg-0"
+      style={{ maxHeight: '100px' }}
+      navbarScroll
+    >
+      <Nav.Link>Home</Nav.Link>
+      <Nav.Link>Register</Nav.Link>
+      <Nav.Link>Login</Nav.Link>
+      <Nav.Link>Search</Nav.Link>
+    </Nav>
+
+  </Navbar.Collapse>
+</Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
